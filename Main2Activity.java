ae19d14d89061e2c6a7c46815add288756b33bdb
Tasnuva Tabassum oshin <tasnuva.oshin12@gmail.com>
2019-03-23 01:01:21 +0600

    Create Main2Activity.java

---
+++ Main2Activity.java
@@ -0,0 +1,27 @@
+package oshin.tasnuva.mysql;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        imageView = findViewById(R.id.next);
+        String link = getIntent().getStringExtra("img");
+
+        Picasso.get().load(link).into(imageView);
+
+
+
+
+    }
+}
